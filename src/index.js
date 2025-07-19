a3d7c3d2578039d78ea99c16eedec2da314f41c0
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-07-19 20:35:20 +0200

    add: starter files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import "./styles/styles.scss";
 
-import './scripts/components/nav.js';
-
-
-alert('teeeeeestttqw23w23w23w23w23w2dsgfd6754y6rthgfhgfghchgcgfcgfdg3w23w23w23w23w23w23w23w23w23w23w23wqwedeeerdgfdfgdgfdgfdfgcxzcxzczcxz')
+import "./scripts/components/weather.js";

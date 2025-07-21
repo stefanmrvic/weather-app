6513c7a708c4d4d35ef587d8861c0b523f5c62d1
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-07-21 03:17:13 +0200

    add: weather icons

--- src/scripts/components/weather.js
+++ src/scripts/components/weather.js
@@ -29,26 +29,91 @@ function fetchWeather(e) {
     generateNext7Days(url);
 }
 
+// Logic for populating images (if/else ifs)
+function findWeatherImage(icon) {
+    const icons = {
+        snow: './assets/SVG/snow.svg',
+        'snow-showers-day': 'snow',
+        'snow-showers-night': 'snow',
+        'thunder-rain': 'lightning',
+        'thunder-showers-day': 'lightning',
+        'thunder-showers-night': 'lightning',
+        rain: 'rainy',
+        'showers-day': 'rainy',
+        'showers-night': 'rainy',
+        fog: 'mist',
+        wind: 'wind',
+        cloudy: 'cloudy',
+        'partly-cloudy-day': 'cloudy-day',
+        'partly-cloudy-night': 'cloudy-night',
+        'clear-day': 'sun',
+        'clear-night': 'moon',
+    };
+
+    return icons[icon];
+}
+
 function generateNext7Days(url) {
     const mondayTemperature = document.querySelector('.monday__temperature');
+    const mondayImg = document.querySelector('.monday__img');
     const tuesdayTemperature = document.querySelector('.tuesday__temperature');
+    const tuesdayImg = document.querySelector('.tuesday__img');
     const wednesdayTemperature = document.querySelector('.wednesday__temperature');
+    const wednesdayImg = document.querySelector('.wednesday__img');
     const thursdayTemperature = document.querySelector('.thursday__temperature');
+    const thursdayImg = document.querySelector('.thursday__img');
     const fridayTemperature = document.querySelector('.friday__temperature');
+    const fridayImg = document.querySelector('.friday__img');
     const saturdayTemperature = document.querySelector('.saturday__temperature');
+    const saturdayImg = document.querySelector('.saturday__img');
     const sundayTemperature = document.querySelector('.sunday__temperature');
+    const sundayImg = document.querySelector('.sunday__img');
 
     const metricUnit = switchBtn.checked ? ' °F' : ' °C';
 
     return fetch(url, { mode: 'cors' })
         .then((response) => response.json())
         .then((response) => {
+            const mondayIcon = findWeatherImage(response.days[0].icon);
+            import(`/src/assets/SVG/${mondayIcon}.svg`).then(
+                (src) => (mondayImg.src = src.default)
+            );
             mondayTemperature.textContent = response.days[0].temp + metricUnit;
+
+            const tuesdayIcon = findWeatherImage(response.days[1].icon);
+            import(`/src/assets/SVG/${tuesdayIcon}.svg`).then(
+                (src) => (tuesdayImg.src = src.default)
+            );
             tuesdayTemperature.textContent = response.days[1].temp + metricUnit;
+
+            const wednesdayIcon = findWeatherImage(response.days[2].icon);
+            import(`/src/assets/SVG/${wednesdayIcon}.svg`).then(
+                (src) => (wednesdayImg.src = src.default)
+            );
             wednesdayTemperature.textContent = response.days[2].temp + metricUnit;
+
+            const thursdayIcon = findWeatherImage(response.days[3].icon);
+            import(`/src/assets/SVG/${thursdayIcon}.svg`).then(
+                (src) => (thursdayImg.src = src.default)
+            );
             thursdayTemperature.textContent = response.days[3].temp + metricUnit;
+
+            const fridayIcon = findWeatherImage(response.days[4].icon);
+            import(`/src/assets/SVG/${fridayIcon}.svg`).then(
+                (src) => (fridayImg.src = src.default)
+            );
             fridayTemperature.textContent = response.days[4].temp + metricUnit;
+
+            const saturdayIcon = findWeatherImage(response.days[5].icon);
+            import(`/src/assets/SVG/${saturdayIcon}.svg`).then(
+                (src) => (saturdayImg.src = src.default)
+            );
             saturdayTemperature.textContent = response.days[5].temp + metricUnit;
+
+            const sundayIcon = findWeatherImage(response.days[6].icon);
+            import(`/src/assets/SVG/${sundayIcon}.svg`).then((src) => {
+                sundayImg.src = src.default;
+            });
             sundayTemperature.textContent = response.days[6].temp + metricUnit;
         })
         .catch((error) => console.log(error));
@@ -60,6 +125,7 @@ function generateWeatherInfoLeft(url) {
     const weatherDate = document.querySelector('.weather__date');
     const weatherTime = document.querySelector('.weather__time');
     const weatherTemperature = document.getElementById('temperature');
+    const weatherImg = document.getElementById('icon');
 
     const metricUnitTemperature = switchBtn.checked ? ' °F' : ' °C';
 
@@ -75,6 +141,11 @@ function generateWeatherInfoLeft(url) {
             weatherDate.textContent = response.days[0].datetime;
             weatherTime.textContent = `${hour}:${minutes}`;
             weatherTemperature.textContent = response.days[0].temp + metricUnitTemperature;
+
+            const weatherIcon = findWeatherImage(response.days[0].icon);
+            import(`/src/assets/SVG/${weatherIcon}.svg`).then((src) => {
+                weatherImg.src = src.default;
+            });
         })
         .catch((error) => console.log(error));
 }
